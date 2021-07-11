d24710f5272089915210104ca7ae4a3225f49853
John Rubio <johnrubio27@gmail.com>
2021-07-11 09:54:53 -0500

    Formulario con verificacion de: Vacio Impar Par SOlo deja ingresar a y b.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'AutomataPilaPalindromePar';
+
+  arrayInput!: string[];
+
+  // Con la siguiente expresion regular solo dejamos ingresar el lenguaje definido a y b
+  blockSpace: RegExp = /[ab]/; 
+
+
+  formAutomata: FormGroup = this.fb.group({
+    inputs: ['', [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private messageService: MessageService
+  ) {}
+
+  capturarArray(): void{
+    const {inputs}: {inputs: string} = this.formAutomata.value;
+    if(inputs.length === 0){
+      console.log('Esta vacio')
+      this.showMessage('error', 'Error', 'No ha ingresado ninguna entrada.')
+    }else if(inputs.length % 2 != 0){
+      console.log('Es impar');
+      this.showMessage('error', 'Error', 'La entrada que ingreso es Impar, solo resolvemos par.')
+    }else {
+      this.arrayInput = inputs.trim().split('');
+      this.showMessage('success', 'Comenzemos', 'Empezara a resolver el automata para verificar si es palindrome.')
+      console.log(this.arrayInput)
+    }
+  }
+
+  showMessage(severity: string,summary: string, detail: string) {
+    this.messageService.add({key: 'bc', severity, summary, detail});
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EstadosComponent } from './components/estados/estados.component';
 import { PilaComponent } from './components/pila/pila.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { PilaComponent } from './components/pila/pila.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PrimeNgModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
